3ea7c39bb8ba1ad0151ee5c7d3c63ee100d91c98
Developer <developer@Mac-de-Developer.local>
2020-05-24 01:44:46 -0700

    Setting Up and Displaying Lists  React Native & Firebase Todo App

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import Colors from "./app/customs/Colors";
+import tempData from "./app/data/tempData";
+import { AntDesign } from "@expo/vector-icons";
+import TodoList from "./app/components/TodoList";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flexDirection: "row" }}>
+        <View style={styles.dividir} />
+        <Text style={styles.title}>
+          TODO
+          <Text style={{ fontWeight: "300", color: Colors.blue }}> |APP</Text>
+        </Text>
+        <View style={styles.dividir} />
+      </View>
+      <View style={{ marginVertical: 48 }}>
+        <TouchableOpacity style={styles.addList}>
+          <AntDesign name="plus" size={16} color={Colors.blue} />
+        </TouchableOpacity>
+
+        <Text style={styles.add}>ADD</Text>
+      </View>
+
+      <View style={{ height: 275, paddingLeft: 32 }}>
+        <FlatList
+          data={tempData}
+          keyExtractor={(item) => item.name}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => <TodoList list={item} />}
+        />
+      </View>
     </View>
   );
 }
@@ -12,8 +45,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //marginTop: -700,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dividir: {
+    backgroundColor: Colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "800",
+    color: Colors.black,
+    paddingHorizontal: 64,
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: Colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  add: {
+    color: Colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8,
   },
 });
---
+++ app/data/tempData.js
@@ -0,0 +1,46 @@
+export default tempData = [
+  {
+    name: "Plan",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Passport check",
+        completed: false,
+      },
+      {
+        title: "Book",
+        completed: true,
+      },
+      {
+        title: "Reserve",
+        completed: false,
+      },
+      {
+        title: "Languague",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "Errands",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Buy Milk",
+        completed: false,
+      },
+      {
+        title: "Camps",
+        completed: true,
+      },
+      {
+        title: "Tik Tok",
+        completed: true,
+      },
+      {
+        title: "Pack dek",
+        completed: true,
+      },
+    ],
+  },
+];
